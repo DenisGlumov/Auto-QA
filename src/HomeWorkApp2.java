74c00df72c4263ebef77928a4c000eae55c95c8a
DenisGlumov <den.glumov@gmail.com>
2021-09-10 22:41:16 +0700

    Homework2

---
+++ src/HomeWorkApp2.java
@@ -0,0 +1,9 @@
+class HomeWorkApp2 {
+    static void main (String[] args) {
+        sumNumbers(5,6);
+    }
+
+    static void sumNumbers(int a, int b) {
+        System.out.println(10 < a + b && a + b  <= 20? "true" : "false");
+    }
+}
