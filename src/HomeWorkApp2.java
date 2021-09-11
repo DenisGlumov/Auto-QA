d1ea413d03df51c922b58e62b1224b53854b1fcc
DenisGlumov <den.glumov@gmail.com>
2021-09-11 22:55:27 +0700

    Homework2

--- src/HomeWorkApp2.java
+++ src/HomeWorkApp2.java
@@ -1,6 +1,14 @@
+/**
+ * HomeWorkApp2
+ *
+ * @author Denis Glumov
+ *
+ * @version 11.09.2021
+ */
+
 class HomeWorkApp2 {
-    public static void main (String[] args) {
-        System.out.println(sumNumbers(5,5 ));
+    public static void main(String[] args) {
+        System.out.println(sumNumbers(5, 5));
         System.out.println(sumNumbers(6, 5));
         System.out.println(sumNumbers(12, 8));
         System.out.println(sumNumbers(18, 3));
@@ -12,23 +20,41 @@ class HomeWorkApp2 {
         System.out.println(numberBoolean(1));
         numberString("tool", 3);
         numberString("bread", 2);
+        System.out.println(years(2376));
+        System.out.println(years(1600));
+        System.out.println(years(1700));
+        System.out.println(years(1849));
+        System.out.println(years(2104));
+        System.out.println(years(1984));
+
     }
 
     static boolean sumNumbers(int a, int b) {
-        return a + b >10 && a + b<=20? true : false;
-        }
-
-    static void numbers (int a) {
-        System.out.println(a >=0 ? "Положительное" : "Отрицательное");
+        return a + b > 10 && a + b <= 20 ? true : false;
     }
 
-    static boolean numberBoolean (int a) {
-        return a >= 0 ? false : true;
+    static void numbers(int a) {
+        System.out.println(a >= 0 ? "Положительное" : "Отрицательное");
     }
 
-    static void numberString (String a, int b) {
+    static boolean numberBoolean(int a) {
+        return a < 0 ? true : false;
+    }
+
+    static void numberString(String a, int b) {
         for (int i = 0; i < b; i++) {
             System.out.println(a);
         }
     }
+
+    static boolean years (int a) {
+        if (a % 4 == 0 && a % 100 != 0 && a % 400 != 0)
+            return true;
+
+        if (a % 4 == 0 && a % 100 == 0 && a % 400 == 0)
+            return true;
+
+        return false;
+    }
+
 }
