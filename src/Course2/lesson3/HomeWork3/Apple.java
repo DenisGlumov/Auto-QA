ed9d3514fc08817710fd2e0b3db5cfd266715bec
DenisGlumov <den.glumov@gmail.com>
2021-12-01 23:06:42 +0700

    Revert "HomeWork3"
    This reverts commit f07795d74f24898aa794149457246fc10297bc42.

--- src/Course2/lesson3/HomeWork3/Apple.java
+++
@@ -1,8 +0,0 @@
-package Course2.lesson3.HomeWork3;
-
-public class Apple extends Fruit {
-
-    public Apple() {
-        super(1.0f);
-    }
-}
--- src/Course2/lesson3/HomeWork3/Orange.java
+++
@@ -1,8 +0,0 @@
-package Course2.lesson3.HomeWork3;
-
-public class Orange  extends Fruit {
-
-    public Orange() {
-        super(1.5f);
-    }
-}
